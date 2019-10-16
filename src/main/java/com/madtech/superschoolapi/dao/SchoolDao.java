1b5888e5d3fef2caa5ef74aa38d515bd83fc4d3b
Madhan-Balaji <madhanb69@gmail.com>
2019-10-16 09:12:28 +0530

    dao - school - insert and fetch update

--- src/main/java/com/madtech/superschoolapi/dao/SchoolDao.java
+++ src/main/java/com/madtech/superschoolapi/dao/SchoolDao.java
@@ -1,12 +1,19 @@
 package com.madtech.superschoolapi.dao;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.sql.Date;
 import java.util.UUID;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.madtech.superschoolapi.models.School;
 import com.madtech.superschoolapi.util.Database;
 
@@ -16,10 +23,27 @@ public class SchoolDao {
 	private Connection connection;
 	private Statement statement;
 	private PreparedStatement preparedStatement;
+	private CallableStatement callableStatement;
 
+	public static void main(String args[]) {
+		SchoolDao dao = new SchoolDao();
+		dao.getAllSchool();
+		ObjectMapper m = new ObjectMapper();
+		School schools = dao.getSchoolById("9d72efb5-13db-4d70-971c-dd1df3411ec5");
+		try {
+			System.out.println(m.writeValueAsString(schools));
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public SchoolDao() {
 		this.db = new Database();
 		this.connection = db.connect();
+		this.preparedStatement = null;
+		this.callableStatement = null;
 
 		try {
 			this.statement = this.connection.createStatement();
@@ -66,7 +90,7 @@ public class SchoolDao {
 			school.setCity(rs.getString("city"));
 			school.setPincode(rs.getInt("pincode"));
 			school.setCreatedBy(rs.getString("createdBy"));
-			school.setUpdatedBy(rs.getString("updateBy"));
+			school.setUpdatedBy(rs.getString("updatedBy"));
 			school.setCreatedDateTime(rs.getTimestamp("createdDateTime"));
 			school.setUpdatedDateTime(rs.getTimestamp("updatedDateTime"));
 		} catch (SQLException e) {
@@ -76,6 +100,24 @@ public class SchoolDao {
 		return school;
 	}
 
+	public ArrayList<School> getAllSchool(){
+		String query;
+		ResultSet resultSet;
+		ArrayList<School> schools = new ArrayList<School>();
+		try {
+			query = "SELECT * FROM school";
+			resultSet = this.statement.executeQuery(query);
+			while (resultSet.next()) {
+				schools.add(this.mapSchoolData(resultSet));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return schools;
+		
+	}
+	
 	public School getSchoolById(String id) {
 		
 		String query;
@@ -83,8 +125,8 @@ public class SchoolDao {
 		School school = null;
 		
 		try {
-			query = "SELECT * FROM school WHERE id = " + id;
-			resultSet = statement.executeQuery(query);
+			query = "SELECT * FROM school WHERE id = '" + id + "'";
+			resultSet = this.statement.executeQuery(query);
 			school = this.mapSchoolData(resultSet);
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -93,16 +135,53 @@ public class SchoolDao {
 		return school;
 	}
 	
-//	public boolean insertSchool(School school) {
-//		String query;
-//		
-//		try {
-//			query = "INSERT INTO school" +
-//					"(id, name, board, startedDate, joinedDate, website, primaryContact, secondaryContact, primaryEmail, secondaryEmail, languages,) VALUES ()";
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public boolean insertSchool(School school) {
+		String query;
+		boolean result = false;
+		try {
+			query = "{CALL insertSchool(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}";
+			
+			this.callableStatement = connection.prepareCall(query);
+			this.callableStatement.setString(1, this.generateSchoolId());
+			this.callableStatement.setString(2, school.getName());
+			this.callableStatement.setString(3, school.getBoard());
+			this.callableStatement.setDate(4, school.getStartedDate());
+			this.callableStatement.setDate(5, school.getJoinedDate());
+			this.callableStatement.setString(6, school.getWebsite());
+			this.callableStatement.setDouble(7, school.getPrimaryContact());
+			this.callableStatement.setDouble(8, school.getSecondaryContact());
+			this.callableStatement.setString(9, school.getPrimaryEmail());
+			this.callableStatement.setString(10, school.getSecondaryEmail());
+			this.callableStatement.setString(11, school.getLanguages());
+			this.callableStatement.setBoolean(12, school.isKinderGardenAvailable());
+			this.callableStatement.setBoolean(13, school.isPrimaryAvailable());
+			this.callableStatement.setBoolean(14, school.isSecondaryAvailable());
+			this.callableStatement.setBoolean(15, school.isHigherSecondaryAvailable());
+			this.callableStatement.setBoolean(16, school.isGovernmentAided());
+			this.callableStatement.setBoolean(17,  school.isInternational());
+			this.callableStatement.setBoolean(18, school.isSmartSchool());
+			this.callableStatement.setString(19, school.getDescription());
+			this.callableStatement.setString(20, school.getLogo());
+			this.callableStatement.setString(21, school.getImage1());
+			this.callableStatement.setString(22, school.getImage2());
+			this.callableStatement.setString(23, school.getImage3());
+			this.callableStatement.setString(24, school.getDoorNo());
+			this.callableStatement.setString(25, school.getStreet());
+			this.callableStatement.setString(26, school.getArea());
+			this.callableStatement.setString(27, school.getCity());
+			this.callableStatement.setInt(28, school.getPincode());
+			this.callableStatement.setString(29, school.getCreatedBy());
+			this.callableStatement.setString(30, school.getUpdatedBy());
+			this.callableStatement.setTimestamp(31, school.getCreatedDateTime());
+			this.callableStatement.setTimestamp(32, school.getUpdatedDateTime());
+			
+			result = this.callableStatement.execute();
+			this.callableStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 	@Override
 	protected void finalize() throws Throwable {
--- src/main/java/com/madtech/superschoolapi/models/School.java
+++ src/main/java/com/madtech/superschoolapi/models/School.java
@@ -1,7 +1,7 @@
 package com.madtech.superschoolapi.models;
 
 import java.sql.Timestamp;
-import java.util.Date;
+import java.sql.Date;
 
 public class School {
 
@@ -37,198 +37,272 @@ public class School {
 	private String updatedBy;
 	private Timestamp createdDateTime;
 	private Timestamp updatedDateTime;
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getBoard() {
 		return board;
 	}
+
 	public void setBoard(String board) {
 		this.board = board;
 	}
+
 	public Date getStartedDate() {
 		return startedDate;
 	}
+
 	public void setStartedDate(Date startedDate) {
 		this.startedDate = startedDate;
 	}
+
 	public Date getJoinedDate() {
 		return joinedDate;
 	}
+
 	public void setJoinedDate(Date joinedDate) {
 		this.joinedDate = joinedDate;
 	}
+
 	public String getWebsite() {
 		return website;
 	}
+
 	public void setWebsite(String website) {
 		this.website = website;
 	}
+
 	public double getPrimaryContact() {
 		return primaryContact;
 	}
+
 	public void setPrimaryContact(double primaryContact) {
 		this.primaryContact = primaryContact;
 	}
+
 	public double getSecondaryContact() {
 		return secondaryContact;
 	}
+
 	public void setSecondaryContact(double secondaryContact) {
 		this.secondaryContact = secondaryContact;
 	}
+
 	public String getPrimaryEmail() {
 		return primaryEmail;
 	}
+
 	public void setPrimaryEmail(String primaryEmail) {
 		this.primaryEmail = primaryEmail;
 	}
+
 	public String getSecondaryEmail() {
 		return secondaryEmail;
 	}
+
 	public void setSecondaryEmail(String secondaryEmail) {
 		this.secondaryEmail = secondaryEmail;
 	}
+
 	public String getLanguages() {
 		return languages;
 	}
+
 	public void setLanguages(String languages) {
 		this.languages = languages;
 	}
+
 	public boolean isKinderGardenAvailable() {
 		return kinderGardenAvailable;
 	}
+
 	public void setKinderGardenAvailable(boolean kinderGardenAvailable) {
 		this.kinderGardenAvailable = kinderGardenAvailable;
 	}
+
 	public boolean isPrimaryAvailable() {
 		return primaryAvailable;
 	}
+
 	public void setPrimaryAvailable(boolean primaryAvailable) {
 		this.primaryAvailable = primaryAvailable;
 	}
+
 	public boolean isSecondaryAvailable() {
 		return secondaryAvailable;
 	}
+
 	public void setSecondaryAvailable(boolean secondaryAvailable) {
 		this.secondaryAvailable = secondaryAvailable;
 	}
+
 	public boolean isHigherSecondaryAvailable() {
 		return higherSecondaryAvailable;
 	}
+
 	public void setHigherSecondaryAvailable(boolean higherSecondaryAvailable) {
 		this.higherSecondaryAvailable = higherSecondaryAvailable;
 	}
+
 	public boolean isGovernmentAided() {
 		return governmentAided;
 	}
+
 	public void setGovernmentAided(boolean governmentAided) {
 		this.governmentAided = governmentAided;
 	}
+
 	public boolean isInternational() {
 		return international;
 	}
+
 	public void setInternational(boolean international) {
 		this.international = international;
 	}
+
 	public boolean isSmartSchool() {
 		return smartSchool;
 	}
+
 	public void setSmartSchool(boolean smartSchool) {
 		this.smartSchool = smartSchool;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getLogo() {
 		return logo;
 	}
+
 	public void setLogo(String logo) {
 		this.logo = logo;
 	}
+
 	public String getImage1() {
 		return image1;
 	}
+
 	public void setImage1(String image1) {
 		this.image1 = image1;
 	}
+
 	public String getImage2() {
 		return image2;
 	}
+
 	public void setImage2(String image2) {
 		this.image2 = image2;
 	}
+
 	public String getImage3() {
 		return image3;
 	}
+
 	public void setImage3(String image3) {
 		this.image3 = image3;
 	}
+
 	public String getDoorNo() {
 		return doorNo;
 	}
+
 	public void setDoorNo(String doorNo) {
 		this.doorNo = doorNo;
 	}
+
 	public String getStreet() {
 		return street;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
+
 	public String getArea() {
 		return area;
 	}
+
 	public void setArea(String area) {
 		this.area = area;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public int getPincode() {
 		return pincode;
 	}
+
 	public void setPincode(int pincode) {
 		this.pincode = pincode;
 	}
+
 	public String getCreatedBy() {
 		return createdBy;
 	}
+
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
 	}
+
 	public String getUpdatedBy() {
 		return updatedBy;
 	}
+
 	public void setUpdatedBy(String updatedBy) {
 		this.updatedBy = updatedBy;
 	}
+
 	public Timestamp getCreatedDateTime() {
 		return createdDateTime;
 	}
+
 	public void setCreatedDateTime(Timestamp createdDateTime) {
 		this.createdDateTime = createdDateTime;
 	}
+
 	public Timestamp getUpdatedDateTime() {
 		return updatedDateTime;
 	}
+
 	public void setUpdatedDateTime(Timestamp updatedDateTime) {
 		this.updatedDateTime = updatedDateTime;
 	}
-	
+
+	public String getValuesString() {
+		String values ="'"+ this.id + "','" + this.name + "','" + this.board + "','" + this.startedDate.toString() + "','" + this.joinedDate.toString()
+				+ "','" + this.website + "'," + this.primaryContact + "," + this.secondaryContact + ",'" + this.primaryEmail
+				+ "','" + this.secondaryEmail + "','" + this.languages + "'," + this.kinderGardenAvailable + ","
+				+ this.primaryAvailable + "," + this.secondaryAvailable + "," + this.higherSecondaryAvailable + ","
+				+ this.governmentAided + "," + this.international + "," + this.smartSchool + ",'" + this.description
+				+ "','" + this.logo + "','" + this.image1 + "','" + this.image2 + "','" + this.image3 + "','" + this.doorNo + "','"
+				+ this.street + "','" + this.area + "','" + this.city + "'," + this.pincode + ",'" + this.createdBy + "','"
+				+ this.updatedBy + "','" + this.createdDateTime + "','" + this.updatedDateTime + "'";
+		return values;
+	}
 }
